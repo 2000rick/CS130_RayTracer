db18ee5eaa1453cc20725a3b0aaa82e8a48b875f
2000rick <qipeng.liang.edu@gmail.com>
2022-02-02 10:17:44 -0800

    minor changes

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -11,12 +11,11 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     vec3 color;
     vec3 ambient = color_ambient*world.ambient_color*world.ambient_intensity;
     color += ambient;
-
-    for(auto i : world.lights)
-    {
-        vec3 lightDirection = i->position-intersection_point;
-        Ray ray1(intersection_point, lightDirection.normalized()); //Ray from intersection to light source
-        if(world.enable_shadows) {
+    
+    if(world.enable_shadows) {
+        for (auto i : world.lights) {
+            vec3 lightDirection = i->position-intersection_point;
+            Ray ray1(intersection_point, lightDirection.normalized()); //Ray from intersection to light source AKA shadow ray
             Hit temp = world.Closest_Intersection(ray1);
             if(temp.object == NULL || temp.dist > lightDirection.magnitude() ) {
                 vec3 emitted = i->Emitted_Light(lightDirection);
@@ -28,10 +27,13 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
                 color += diffuse + specular;
             }
         }
-        else {
+    }
+    else {
+        for (auto i : world.lights) {
+            vec3 lightDirection = i->position-intersection_point;
+            Ray ray1(intersection_point, lightDirection.normalized()); //Shadow ray
             vec3 emitted = i->Emitted_Light(lightDirection);
             vec3 diffuse = color_diffuse*emitted*std::max(dot(normal, lightDirection.normalized()), 0.0);
-
             vec3 r = ( 2*(dot(lightDirection.normalized(), normal))*normal-lightDirection.normalized() ).normalized(); //ReflectedDirection
             vec3 c = (world.camera.position - intersection_point).normalized();
             double intensity = pow(std::max(dot(r, c), 0.0), specular_power);
